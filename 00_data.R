b3bba338f699ed5573e5a2f8115ad4590f0affdb
takewiki <hulilei@takewiki.com.cn>
2020-09-02 23:51:18 +0800

    initial setup

---
+++ 00_data.R
@@ -0,0 +1,21 @@
+# 设置app标题-----
+
+app_title <-'App Template';
+
+# store data into rdbe in the rds database
+app_id <- 'appTpl'
+#app_id <- 'caas'
+
+#设置数据库链接---
+
+conn_be <- conn_rds('rdbe')
+
+
+
+#设置链接---
+conn <- conn_rds('nsic')
+
+
+
+
+
